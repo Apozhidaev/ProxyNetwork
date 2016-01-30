e5abd4aaeebdf3523f3888cf94038199594cad60
alexanderpo <alexander.pozhidaev@gmail.com>
2016-01-31 02:53:02 +0300

    first commit

---
+++ ProxyNetwork/HttpClientPro.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+
+namespace ProxyNetwork
+{
+    public class HttpClientPro : IDisposable
+    {
+        public static HttpClientPro Create(IProxyProvider proxyProvider)
+        {
+            var proxy = proxyProvider.Next();
+            if (proxy == null)
+            {
+                proxyProvider.Refresh().Wait();
+                proxy = proxyProvider.Next();
+                if (proxy == null)
+                {
+                    throw new Exception("The proxy provider had not found available proxy.");
+                }
+            }
+            return new HttpClientPro(proxy);
+        }
+
+        public HttpClientPro(Uri proxy = null)
+        {
+            if (proxy != null)
+            {
+                Proxy = proxy;
+                var handler = new HttpClientHandler
+                {
+                    UseProxy = true,
+                    Proxy = new WebProxy(proxy) { UseDefaultCredentials = false }
+                };
+                Client = new HttpClient(handler);
+            }
+            else
+            {
+                Client = new HttpClient();
+
+            }
+        }
+
+        public HttpClient Client { get; }
+
+        public Uri Proxy { get; }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+        }
+    }
+}
---
+++ ProxyNetwork/IProxyProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ProxyNetwork
+{
+    public interface IProxyProvider
+    {
+        Task<int> Refresh();
+        Uri Next();
+        void Remove(Uri proxy, bool toBlackList);
+    }
+}
